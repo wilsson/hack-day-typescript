aaefac359731962d257285eca52b12424e48e85d
wilsson <wilsonft83@gmail.com>
2017-11-24 17:21:45 -0500

    feat(ts): agregando tipos

---
+++ types/index.js
@@ -0,0 +1,30 @@
+var max = 10;
+// String
+var myString = 'myString';
+var myStringTwo = 'myString';
+// Number
+var myNumber = 10;
+var myNumberTwo = 10;
+// Boolean
+var myBoolean = true;
+var myBooleanTwo = true;
+// Enum
+var Power;
+(function (Power) {
+    Power[Power["maxPower"] = 100] = "maxPower";
+    Power[Power["minPower"] = 10] = "minPower";
+})(Power || (Power = {}));
+// Tuple
+var x;
+x = ['string', 10];
+//Array
+var myArray = [1, 2, 3, 4];
+var myArrayTwo = [1, 2, 3, 4];
+// Void
+function myFunction() {
+    // TODO: code here
+}
+// Never
+function error(message) {
+    throw new Error(message);
+}
---
+++ types/index.ts
@@ -0,0 +1,42 @@
+// String
+let myString: string = 'myString';
+let myStringTwo =  <string>'myString';
+
+// Number
+let myNumber: number = 10;
+let myNumberTwo = <number>10;
+
+// Boolean
+let myBoolean: boolean = true;
+let myBooleanTwo =  <boolean>true;
+
+// Enum
+enum Power {
+    maxPower =  100,
+    minPower = 10
+}
+
+// Tuple
+let x: [string, number];
+
+x = ['string', 10];
+
+//Array
+let myArray: number[] = [1, 2, 3, 4];
+
+let myArrayTwo: Array<number> = [1, 2, 3, 4];
+
+// Void
+function myFunction(): void {
+    // TODO: code here
+}
+
+// Never
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// Type assertion “angle-bracket”
+let value: any = "this is a string";
+
+let strLength: number = (<string>value).length;
